ea1cc7727afbc6ad53852e452782ed75a21c89ba
neworderofjamie <neworderofjamie@gmail.com>
2022-11-01 10:08:48 +0000

    split up name

--- process_output.py
+++ process_output.py
@@ -1,12 +1,16 @@
+import re
+from os import path
+
 from csv import writer
 from glob import glob
 from datetime import timedelta
 
-outputs_filenames = list(glob("output*.txt"))
+outputs_filenames = list(glob("synthetic_output/output*.txt"))
 
+name_parser = re.compile(r"output_(\d+)_([A-Z_]+).txt")
 with open("summary.csv", "w") as csv_file:
     csv_writer = writer(csv_file, delimiter=",")
-    csv_writer.writerow(["Name", "Runner size [mbyte]", "Wall clock time [s]", "Resident set size [mbyte]"])
+    csv_writer.writerow(["Num arrays", "Check CUDA errors implementation", "Runner size [mbyte]", "Wall clock time [s]", "Resident set size [mbyte]"])
     for n in outputs_filenames:
         try:
             with open(n, "r") as f:
@@ -26,7 +30,8 @@ with open("summary.csv", "w") as csv_file:
                         wall_clock_sec = timedelta(hours=int(hours), minutes=int(minutes), seconds=int(seconds)).total_seconds()
                     elif l.startswith("Maximum resident set size (kbytes):"):
                         resident_set_mbyte = int(l[36:]) // 1024
-                csv_writer.writerow([n, runner_mbytes, wall_clock_sec, resident_set_mbyte])
+                name_fields = name_parser.match(path.basename(n))
+                csv_writer.writerow([name_fields.group(1), name_fields.group(2), runner_mbytes, wall_clock_sec, resident_set_mbyte])
         except:
             print(f"Unable to parse {n}")
             

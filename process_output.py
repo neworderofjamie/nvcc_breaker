ab6fd3bed7b42c97fc771644e9e83cab2e261460
neworderofjamie <neworderofjamie@gmail.com>
2022-11-01 13:10:01 +0000

    floating point mbyte calculation

--- process_output.py
+++ process_output.py
@@ -15,7 +15,7 @@ with open("summary.csv", "w") as csv_file:
         try:
             with open(n, "r") as f:
                 runner_ls = f.readline()
-                runner_mbytes = int(runner_ls.split(" ")[4]) // (1024 * 1024)
+                runner_mbytes = int(runner_ls.split(" ")[4]) / (1024 * 1024)
                 
                 # Loop through other lines
                 wall_clock_sec = None
@@ -29,7 +29,7 @@ with open("summary.csv", "w") as csv_file:
                         minutes, seconds = rest.split(".")
                         wall_clock_sec = timedelta(hours=int(hours), minutes=int(minutes), seconds=int(seconds)).total_seconds()
                     elif l.startswith("Maximum resident set size (kbytes):"):
-                        resident_set_mbyte = int(l[36:]) // 1024
+                        resident_set_mbyte = int(l[36:]) / 1024
                 name_fields = name_parser.match(path.basename(n))
                 csv_writer.writerow([name_fields.group(1), name_fields.group(2), runner_mbytes, wall_clock_sec, resident_set_mbyte])
         except:
